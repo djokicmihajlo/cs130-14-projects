7dbdcb568e98b2671afb527a884b36ee448e0c1e
Mihajlo <djokicsoffice@gmail.com>
2025-04-18 10:07:42 +0200

    project 1

--- project-1-calculator/main.c
+++ project-1-calculator/main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int a;
+    int b;
+    char c;
+    int result;
+    printf("Enter the first number:");
+    scanf("%d", &a);
+    printf("Enter the second number:");
+    scanf("%d", &b);
+    printf("Enter the operation (+,-,/,*)");
+    scanf(" %c", &c);
+
+    switch (c) {
+        case '+':
+            result = a + b;
+            printf("Result is %d\n", result);
+            break;
+        case '-':
+            result = a - b;
+            printf("Result is %d\n", result);
+
+            break;
+        case '/':
+            if (b != 0) {
+                result = a / b;
+                printf("Result is %d\n", result);
+            } else {
+                printf("Division by Zero\n");
+            }
+            break;
+        case '*':
+            result = a * b;
+            printf("Result is %d\n", result);
+
+            break;
+        default:
+            printf("Invalid Operation\n");
+            break;
+    }
     return 0;
 }
